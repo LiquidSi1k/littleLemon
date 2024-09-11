f3f474758891c754897536870a13e7cfa6ab1e88
Voo Tzuen Hau <vince_voo@yahoo.com>
2024-09-11 21:49:25 +0800

    meta tags added and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Nav />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="center-all list-none">
+        <a href="/">
+          <li>x</li>
+        </a>
+        <a href="/">
+          <li>y</li>
+        </a>
+        <a href="/">
+          <li>z</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
