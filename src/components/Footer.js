2c8765b89445c1171f1c0fbf2b6c8445d2ca70b8
Voo Tzuen Hau <vince_voo@yahoo.com>
2024-09-23 18:24:16 +0800

    added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { logo } from "../constants/assets";
+import { FaXTwitter } from "react-icons/fa6";
+import { CiInstagram } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="center-all footer">
-      <p>&copy; 2024 Little Lemon</p>
+    <footer className="footer">
+      <div className="flex">
+        <div className="footer-tags">
+          <Link to={"/"} className="list-none">
+            <img src={logo} height={40} width={40} alt="logo" />
+          </Link>
+          <div>
+            <p>Home</p>
+            <p>Order Online</p>
+            <p>Reservation</p>
+          </div>
+          <div>
+            <p>Contact</p>
+            <p>Address</p>
+            <p>Contact Number</p>
+            <p>Email</p>
+          </div>
+          <div className="flex-col">
+            <p>Links</p>
+            <p>
+              <FaXTwitter size={20} />
+            </p>
+            <p>
+              <CiInstagram size={20} />
+            </p>
+            <p>
+              <FaFacebook size={20} />
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="center-all py-20">&copy; 2024 Little Lemon</p>
     </footer>
   );
 };
--- src/components/SpecialsMeals.js
+++ src/components/SpecialsMeals.js
@@ -1,30 +1,48 @@
 import React, { useContext } from "react";
 import { DataContext } from "../context/DataContext";
+import { MdDeliveryDining } from "react-icons/md";
 
 const SpecialsMeals = ({ type }) => {
   const { menu } = useContext(DataContext);
 
+  const generateRandomPrice = (min = 10, max = 20) => {
+    return (Math.random() * (max - min) + min).toFixed(2);
+  };
+
+  if (!menu || menu.length === 0) {
+    return <p>No dishes available at the moment.</p>;
+  }
+
   const filteredData = menu.filter((dish) => dish.mealType.includes(type));
 
   return (
-    <>
+    <div className="specials-container">
       {(filteredData.length > 0 ? filteredData : menu)
         .slice(0, 10)
         .map((dish) => (
-          <div className="specials-section" key={dish.name}>
-            <div>
+          <div className="specials-section " key={dish.name}>
+            <img
+              src={dish.image}
+              height={100}
+              width={100}
+              alt={dish.name || "food"}
+            />
+            <div className="specials-info">
               <h4>{dish.name}</h4>
+              <p>${dish.price || generateRandomPrice()}</p>
               <p>
-                {dish.cuisine} {dish.mealType[0]}{" "}
-                {dish.mealType[1] ? ` / ${dish.mealType[1]}` : ""} made with{" "}
-                {dish.ingredients.slice(0, 4)} &nbsp;
+                {dish.cuisine} {dish.mealType.join(" / ")} made with{" "}
+                {dish.ingredients.slice(0, 4).join(", ")} &nbsp;
               </p>
-              <p>$12.99</p>
+
+              <div className="specials-delivery">
+                <span>Order a delivery</span>
+                <MdDeliveryDining />
+              </div>
             </div>
-            <img src={dish.image} height={100} width={100} alt="food" />
           </div>
         ))}
-    </>
+    </div>
   );
 };
 
